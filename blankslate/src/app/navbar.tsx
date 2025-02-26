8dd84128eb892528fc7fc588b700d90161e643d6
mrmikwebber <mi.webber.ii@gmail.com>
2025-02-25 19:15:45 -0700

    base navbar created

---
+++ blankslate/src/app/navbar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <div>
+      <nav className="block w-full px-4 py-4 mx-auto bg-white bg-opacity-90 sticky top-3 shadow lg:px-8 backdrop-blur-lg backdrop-saturate-150 z-[9999]">
+        <div className="container flex flex-wrap items-center justify-between text-slate-800">
+          <Link
+            href="/"
+            className="mr-4 block cursor-pointer py-1.5 text-teal-600 font-bold text-2xl"
+          >
+            blankslate
+          </Link>
+
+          <div className="lg:hidden">
+            <button
+              className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              onClick={toggleMobileMenu}
+              type="button"
+            >
+              <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </span>
+            </button>
+          </div>
+
+          {/* Desktop Menu */}
+          <div className="hidden lg:block">
+            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+              <li>
+                <button className="bg-teal-600 hover:bg-teal-500 text-white px-8 py-2 rounded-md">
+                  Login
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
