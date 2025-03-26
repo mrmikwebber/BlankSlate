dbe2842e966cae2c879f8e2b9df2dbb38d51646e
mrmikwebber <mi.webber.ii@gmail.com>
2025-03-25 22:29:58 -0700

    Content security policy implemented

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+default-src 'self';
+script-src 'self' 'unsafe-inline';
+style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+font-src 'self' fonts.gstatic.com;
+img-src 'self' data: blob:;
+connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL};
+frame-src 'none';
+object-src 'none';
+base-uri 'self';
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
