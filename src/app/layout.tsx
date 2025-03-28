bd03ee2596970a074d3d5618e5f7fea1ced011f8
mrmikwebber <mi.webber.ii@gmail.com>
2025-03-27 21:15:05 -0700

    Speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Navbar from "./navigation/navbar";
 import { AccountProvider } from "./context/AccountContext";
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <AuthProvider>
           <Navbar />
             <AccountProvider>
