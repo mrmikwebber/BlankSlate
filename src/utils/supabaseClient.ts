b6a3c7899ceb0d5450bfb084fd8c160f8514e718
mrmikwebber <mi.webber.ii@gmail.com>
2025-05-02 12:46:19 -0700

    disable supabase loading on the server

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,13 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-    },
-  }
-);
+let supabase: ReturnType<typeof createClient> | null = null;
+
+if (typeof window !== 'undefined' && !supabase) {
+  supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+    }
+  );
+}
+
+export { supabase };
